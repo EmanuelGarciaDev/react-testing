26edd90b6dfadd3c416f6db4ae6ca1124ff670e2
Emanuel Dario Garcia <emanueldario.dev@gmail.com>
2025-02-01 13:45:58 +0100

    firstTest testing & working

---
+++ src/firstTest.test.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect} from 'vitest';
+
+describe('first test', () => {
+    it('should add', () => {
+        const sum = (a, b) => a + b;
+        const result = sum(2, 3)
+        expect(result).toBe(5);
+    });
+    it('text should be equal', () => {
+        const text = 'Vamo independiente';
+        const text2 = 'Vamos independiente';
+        expect(text).not.toBe(text2);
+    })
+});
