e0c4a23947567212653fba26f1a71642ba005ed5
Emanuel Dario Garcia <emanueldario.dev@gmail.com>
2025-02-04 22:02:29 +0100

    tested login with valid and invalid credentials, and also password visibility toggler tested

--- src/containers/Login/Login.test.tsx
+++ src/containers/Login/Login.test.tsx
@@ -1,19 +1,32 @@
-import { describe, it, expect, vi, Mock } from "vitest";
-import { act, fireEvent, render, screen } from "@testing-library/react";
+import { describe, it, expect, vi, Mock, beforeEach } from "vitest";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { getAuth } from "../../services/getAuth";
 import { SessionProvider } from "../../context/AuthContext";
 import { Login } from "./Login";
 
+vi.mock("react-router-dom", async () => {
+    const actual = await import("react-router-dom");
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate,
+    }
+})
+
 vi.mock("../../services/getAuth", () => ({
-    getAuth: vi.fn()
+  getAuth: vi.fn(),
 }));
 
-const mockGetAuth = getAuth as Mock
+const mockGetAuth = getAuth as Mock;
+const mockNavigate = vi.fn();
 
 describe("<Login />", () => {
-  it("should show error message", async () => {
-    mockGetAuth.mockRejectedValue(new Error("Invalid credentials"));
+    let usernameInput: HTMLInputElement;
+    let passwordInput: HTMLInputElement;
+    let buttonLogin: HTMLButtonElement;
+    let showPassword: HTMLButtonElement;
+    
+  beforeEach(() => {
     render(
       <SessionProvider>
         <MemoryRouter>
@@ -21,15 +34,55 @@ describe("<Login />", () => {
         </MemoryRouter>
       </SessionProvider>
     );
-    const usernameInput = screen.getByPlaceholderText("Username");
-    const passwordInput = screen.getByPlaceholderText("Password");
-    const buttonLogin = screen.getByRole("button", { name: "Login" });
-    await act(() => {
-        fireEvent.change(usernameInput, { target: { value: "wrongUser"}})
-        fireEvent.change(passwordInput, { target: { value: "wrongPassword"}})
-        fireEvent.click(buttonLogin);
-    })
-    const errorMessage = screen.getByText("Invalid credentials");
-    expect(errorMessage).toBeInTheDocument();
+    usernameInput = screen.getByPlaceholderText("Username");
+    passwordInput = screen.getByPlaceholderText("Password");
+    buttonLogin = screen.getByRole("button", { name: "Login" });
   });
+
+it("should show error message", async () => {
+  mockGetAuth.mockRejectedValue(new Error("Invalid credentials"));
+  await act(() => {
+    fireEvent.change(usernameInput, { target: { value: "wrongUser" } });
+    fireEvent.change(passwordInput, { target: { value: "wrongPassword" } });
+    fireEvent.click(buttonLogin);
+  });
+  const errorMessage = screen.getByText("Invalid credentials");
+  expect(errorMessage).toBeInTheDocument();
+});
+
+it("should navigate to /orders", async () => {
+  mockGetAuth.mockResolvedValue({ success: true });
+await act(() => {
+    fireEvent.change(usernameInput, { target: { value: "rightUser" } });
+    fireEvent.change(passwordInput, { target: { value: "rightPassword" } });
+    fireEvent.click(buttonLogin);
+  });
+  await waitFor(() => {
+      expect(mockGetAuth).toHaveBeenCalledWith("rightUser", "rightPassword");
+      expect(mockNavigate).toHaveBeenCalledWith("/orders");
+  })
+});
+
+it("should show password", async () => {
+    //arrange
+    showPassword = screen.getByRole("button", {name: "show"})
+    //act
+    await act(() => {
+        fireEvent.change(passwordInput, {target: {value: "testPassword"}});
+        fireEvent.click(showPassword);
+    })
+    //assert
+    expect(showPassword).toHaveTextContent("hide");
+    expect(passwordInput).toHaveAttribute("type", "text");
+})
+it("should hide password", async () => {
+    showPassword = screen.getByRole("button", {name: "show"})
+    await act(() => {
+        fireEvent.change(passwordInput, {target: {value: "testPassword"}});
+        fireEvent.click(showPassword);
+        fireEvent.click(showPassword);
+    })
+    expect(showPassword).toHaveTextContent("show");
+    expect(passwordInput).toHaveAttribute("type", "password");
+})
 });
